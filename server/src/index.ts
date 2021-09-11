4566e173952061abc05fc8cf9191f375399f5746
Atharva Bhange <atharva.bhange@gmail.com>
2021-09-11 11:33:51 +0530

    now using database url

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,7 @@ import { createConnection } from "typeorm";
 	await createConnection({
 		name: "default",
 		type: "postgres",
-		host: process.env.DB_HOST,
-		port: parseInt(process.env.DB_PORT),
-		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME,
+		url: process.env.DATABASE_URL,
 		synchronize: true,
 		logging: true,
 		entities: ["src/entity/*.*"],
